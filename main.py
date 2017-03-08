87adf8dd1112f77158d937791b8e88dbcbd22519
jpfeiffer16 <jpfeiffer@bluemodus.com>
2017-03-07 22:14:11 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#! /bin/bash
+from bf import bf
+import argparse
+
+parser = argparse.ArgumentParser(description="Command-Line bf runner")
+
+# parser.add_argument()
+
+parser.add_argument("-c", "--code", help = "code to parse")
+parser.add_argument("-i", "--input", help = "input to use")
+
+args = parser.parse_args()
+
+if (args.input == None or args.code == None):
+  print("Must provide code and input")
+
+interpreter = bf(args.code, args.input)
+
+print(interpreter.run())
